d9d31b489dd64d8136e83e511334fe844d2b91da
rrchaos <raul.moragavillar@kedgebs.com>
2019-08-13 10:01:29 +0200

    route :param added as well as query=

--- node/sampleTurboProjectGenerated/routes/index.js
+++ node/sampleTurboProjectGenerated/routes/index.js
@@ -10,24 +10,10 @@ router.get('/', (req, res) => {
 	res.render('index', {text: 'This is the dynamic data. Open index.js from the routes directory to see.'})
 })
 
-/*  This route render json data */
-router.get('/json', (req, res) => {
-	res.json({
-		confirmation: 'success',
-		app: process.env.TURBO_APP_ID,
-		data: 'this is a sample json route.'
+router.get('/:param', (req, res) => {
+	res.send({
+		"data": "test response"
 	})
 })
-
-/*  This route sends text back as plain text. */
-router.get('/send', (req, res) => {
-	res.send('This is the Send Route')
-})
-
-/*  This route redirects requests to Turbo360. */
-router.get('/redirect', (req, res) => {
-	res.redirect('https://www.turbo360.co/landing')
-})
-
 
 module.exports = router
