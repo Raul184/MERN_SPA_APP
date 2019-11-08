2ab75a3b97d23728778a378ff6d85700a39f8af9
Snash <raulmv01@hotmail.es>
2019-11-08 21:13:47 +0000

    slugify added

--- node/node-farm/index.js
+++ node/node-farm/index.js
@@ -2,7 +2,8 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
-
+//third party
+const slugify = require('slugify');
 //Own Mods.
 const replaceTemplate = require('./modules/replaceTemplate');
 
@@ -15,7 +16,7 @@ const rootProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json` , 'utf-8');
 const dataObj = JSON.parse(data);
 
-
+// const slugs = dataObj.map( el => slugify( el.productName , { lowercase: true }))
 
 const server = http.createServer( (req , res) => {
   const { query , pathname } = url.parse( req.url , true);
