0e564136ac5a9e0574c93cf30011ce08fadf379f
Snash <raulmv01@hotmail.es>
2020-01-19 17:29:36 +0100

    routing for tours functionalities completed

--- node/natureTours/routes/tours.js
+++ node/natureTours/routes/tours.js
@@ -3,12 +3,12 @@ const fs = require('fs');
 const express = require('express');
 const router= express.Router();
 
-//Get all Tours             GET 
-//public
 const tours= JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
-// console.log(tours);
 
-router.get('/' , (req , res) => {
+// @Route           / 
+// @Description     Get all tours
+// @Access          Public
+const getAllTours = (req , res) => {
   res.status(200).json({
     status: 'Success',
     results: tours.length,
@@ -16,13 +16,105 @@ router.get('/' , (req , res) => {
       tours
     }
   })
-})
+} 
 
-router.post('/' , (req , res) => {
+
+// @Route           /:id 
+// @Description     Get 1 tour
+// @Access          Public
+const getATour = (req , res) => {
+  const idTour = tours.find( el => el.id === JSON.parse(req.params.id) )
+  
+  // First check for any malicious code
+  // then
+  if(!idTour){
+    return res.status(404).json({
+      msg: `Sorry , we couldn't find a tour under this id`
+    })
+  }
+  res.status(200).json({
+    status: 'Success',
+    data: {
+      idTour
+    }
+  });
+}
+
+
+
+// @Route           /:id 
+// @Description     Update 1 tour
+// @Access          Private
+const updateTour = (req , res) => {
+  const updatedTour = tours.find( el => el.id === JSON.parse(req.params.id) )
+  // First check for any malicious code
+  // then
+  if(!updatedTour){
+    return res.status(404).json({
+      msg: `Sorry , we couldn't find a tour`
+    })
+  }
+  const { 
+    name , duration , maxGroupSize , difficulty , ratingsAverage , ratingsQuantity , price , 
+    summary , description , imageCover , images , startDates
+  } = req.body; 
+  
+  //first check for any malicious code
+  // then
+  if(name) { updatedTour.name = name}
+  if(duration) { updatedTour.duration = duration}
+  if(maxGroupSize) { updatedTour.maxGroupSize = maxGroupSize}
+  if(difficulty) { updatedTour.difficulty = difficulty}
+  if(ratingsAverage) { updatedTour.ratingsAverage = ratingsAverage}
+  if(ratingsQuantity) { updatedTour.ratingsQuantity = ratingsQuantity}
+  if(price) { updatedTour.price = price}
+  if(summary) { updatedTour.summary = summary}
+  if(description) { updatedTour.description = description}
+  if(imageCover) { updatedTour.imageCover = imageCover}
+  if(images) { updatedTour.images = images}
+  if(startDates) { updatedTour.startDates = startDates}
+  
+  tours.map( (el , i) => {
+    if( el.id === JSON.parse(req.params.id) ){
+      tours[i] = updatedTour;
+    } 
+  })
+
+  res.status(200).json({
+    status: 'Success',
+    msg: 'Tour updated'
+  });
+}
+
+
+// @Route           /:id
+// @Description     DELETE a  tour
+// @Access          Private
+
+const deleteTour = ( req , res ) => {
+  if(req.params.id * 1 > tours.length ){
+    res.status(404).json({
+      msg: 'Invalid Id'
+    })
+  }
+  tours = tours.filter(el => el.id !== JSON.parse(req.params.id) )
+  fs.writeFile( 
+    `${__dirname}/../dev-data/data/tours-simple.json` , 
+    JSON.parse(tours) ,
+    err => {
+      res.status(200).json({ msg: "Tour deleted" })
+    }
+  )
+}
+
+// @Route           /:tour 
+// @Description     Add a new tour
+// @Access          Private
+const addTour = (req , res) => {
   const nueId = tours[tours.length - 1].id + 1;
   const nueTour = Object.assign({ id: nueId} , req.body);
   tours.push(nueTour);
-
+  
   fs.writeFile(
     `${__dirname}/../dev-data/data/tours-simple.json` , 
     JSON.parse(tours) , 
@@ -35,6 +127,14 @@ router.post('/' , (req , res) => {
       })
     } 
   )
-});
+}
+  
+router.get( '/' , getAllTours )
+router.get( '/:id' , getATour );
+router.put( '/:id' , updateTour )
+router.delete( '/:id' , deleteTour )
+router.post( '/:tour' , addTour);
+  
+  
 
 module.exports = router;
