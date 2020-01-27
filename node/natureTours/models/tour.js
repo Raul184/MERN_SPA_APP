42f479f4e03255fdeeaa04bf3a06be5d9ce726ca
Snash <raulmv01@hotmail.es>
2020-01-27 19:15:29 +0100

    Tour model prepared

---
+++ node/natureTours/models/tour.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+
+const tour = new mongoose.Schema({
+  name: {
+    type: String ,
+    required: [ true , `Please , don't forget to name the tour`] ,
+    unique: true
+  },
+  rating: {
+    type: Number ,
+    default: 4.5
+  },
+  price: {
+    type: Number ,
+    required: [ true , `Please don't forget to price the your`]
+  }
+});
+
+
+const TourModel = mongoose.Model( 'Tour' , tour );
+
+module.exports = TourModel;
