b7e4a476a7559683b238146819cd31825e671730
Snash <raulmv01@hotmail.es>
2020-02-29 17:13:14 +0100

    update on errorHandler for dev mode

--- node/natureTours/controllers/errorController.js
+++ node/natureTours/controllers/errorController.js
@@ -2,8 +2,27 @@ module.exports = (err , req , res , next ) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  return res.status(err.statusCode).json({
-    status: err.status ,
-    message: err.message
-  })
+  if(process.env.NODE_ENV === 'development'){
+    return res.status(err.statusCode).json({
+      status: err.status ,
+      err: err ,
+      message: err.message ,
+      stack: err.stack
+    });
+  }
+  // Operational Errors 
+  if(process.env.NODE_ENV === 'production' && err.isOperational ){
+    return res.status(err.statusCode).json({
+      status: err.status ,
+      message: err.message 
+    });
+  } // Programming Errors
+  else if(process.env.NODE_ENV === 'production')
+  {
+    console.error(err)   
+    res.status(500).json({
+      status: 'error',
+      message: 'Something wrong just happened , sorry'
+    })
+  }
 }
