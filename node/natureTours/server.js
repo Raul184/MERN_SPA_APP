b13946cb3c6d8b14bd3a618a94a52e00581e54c2
Snash <raulmv01@hotmail.es>
2020-04-28 16:58:26 +0100

    Security best practices vs bruteForce attacks/NoSQLInjections/xss/rate-limiting implemented

--- node/natureTours/server.js
+++ node/natureTours/server.js
@@ -3,25 +3,65 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
+// Security 
+const rateLimit = require('express-rate-limit');
+const helmet= require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss= require('xss-clean');
+const hpp = require('hpp');
+
+
 // To work With Node Environmnet Vars => process.env.NODE_ENV
 dotenv.config({ path: './config.env'});
 const app = express();
 const AppError = require('./utils/ErrorHandler');
 const globalErrorHandler = require('./controllers/errorController');
 
-//Middlewares =====================
+// GLOBAL Middlewares =====================
+
+// 4 
+app.use(helmet())
+
+// 1 
 if(process.env.NODE_ENV === 'development'){
   app.use(morgan('dev'));
 }
+// 2 Rate limiting 
+const limiter = rateLimit({
+  max: 100 ,
+  windowMs: 60 * 60 * 1000 ,
+  message: 'Too many requests from this Ip, please try again after'
+})
+app.use( '/api' , limiter ) 
 
-// MIDDLEWARES
-// req/res cycle
-app.use(express.json());
-// // static Files
-// app.use(express.static(`${__dirname}/public`))
+// 3 req/res => {}
+app.use(express.json({ limit: '10kb' }));
 
+// 4 Data Sanitazitation vs NOSQL query injections
+app.use(mongoSanitize());
 
-// DB
+// 5 Data Sanitazitation vs XSS (Cross-site scripting attack)
+app.use(xss());
+
+// 6 Parameter Pollutions
+app.use(hpp({ 
+  whitelist: [
+    'duration',
+    'ratingsQuantity' ,
+    'ratingsAverage' ,
+    'maxGroupSize' ,
+    'difficulty' ,
+    'price'
+  ]
+}));
+
+// Tests || Debugging purposes
+app.use( (req , res, next) => {
+  req.requestTime = new Date().toISOString;
+  // console.log(req.headers);
+})
+
+// DB  ==========================================
 mongoose.connect( process.env.MONGO_URI , {
   useNewUrlParser: true ,
   useUnifiedTopology: true ,
@@ -30,18 +70,20 @@ mongoose.connect( process.env.MONGO_URI , {
 }).then(
   () => console.log('DB plugged')
 )
+
 // PORT
 const PORT = process.env.PORT || 5000; 
 
-
-//homepage
+// root
 app.get('/' , (req , res) => {
   res.status(200)
      .send({
        msg: 'Server running..'
      })
 });
-// ROUTES
+
+
+// ROUTES  ==========================================
 app.use('/api/v1/tours' , require('./routes/tourRoutes.js'));
 app.use('/api/v1/users' , require('./routes/userRoutes.js'))
 // Invalid Routes
