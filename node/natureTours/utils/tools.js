2572285c374accc986e1a7a92de637b51cd01ef5
Snash <raulmv01@hotmail.es>
2020-04-28 12:58:40 +0100

    JWT send via cookies completed

--- node/natureTours/utils/tools.js
+++ node/natureTours/utils/tools.js
@@ -20,6 +20,21 @@ const signYsendToken = (statusCode , user , res , sent=true ) => {
     process.env.JWT_SECRET ,
     { expiresIn: process.env.JWT_EXPIRES_IN }
   )
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN *24 * 60 * 60 * 1000 
+    ) ,
+    httpOnly: true // Browsers just -> receive and forward with every req
+  }
+  if(process.env.NODE_ENV === 'production') {
+    // Send only via HTTPS
+    cookieOptions.secure= true
+  }
+
+  // JWT via Cookies 
+  res.cookie('jwt', token , cookieOptions )
+
+  user.password = undefined
   return res.status(statusCode).json({
     status: 'success',
     token : sent ? token : '' ,
