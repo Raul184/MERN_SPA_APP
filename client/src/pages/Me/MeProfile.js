f990a7b2fb70e82fef6317efb17a6e2462b250eb
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-06-29 14:42:53 +0100

    refactoring on profilePage to redux-hooks

--- client/src/pages/Me/MeProfile.js
+++ client/src/pages/Me/MeProfile.js
@@ -1,39 +1,36 @@
 import React from 'react';
 import './meProfile.style.scss';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import { useSelector } from 'react-redux';
 import { grabProfile, grabLoading } from '../../redux/users/user.selectors';
 import AdminNav from '../../components/adminNav/adminNav'
 import ProfileNav from '../../components/profileNav/ProfileNav'
 import ProfileForm from '../../components/profileForm/ProfileForm'
 import PasswordForm from '../../components/passwordForm/PasswordForm'
 import Spinner from '../../components/spinner/Spinner'
-const MeProfile = ({ profile, loading }) => loading ? 
-  <Spinner /> : 
-  <div className="main">
-  <div className="user-view">
-    <nav className="user-view__menu">
-      <ProfileNav/>
-      {profile !== null && profile.role === 'admin' && <AdminNav/>}
-    </nav>
-    <div className="user-view__content">
-      <div className="user-view__form-container">
-        <h2 className="heading-secondary ma-bt-md">Your account settings</h2>
-        <ProfileForm profile={profile} />
-      </div>
-      <div className="line">&nbsp;</div>
-      <div className="user-view__form-container">
-        <h2 className="heading-secondary ma-bt-md">Password change</h2>
-        <PasswordForm />
+const MeProfile = () => {
+  const profile = useSelector(grabProfile)
+  const loading = useSelector(grabLoading)
+  return (
+    loading ? <Spinner /> : 
+    <div className="main">
+      <div className="user-view">
+        <nav className="user-view__menu">
+          <ProfileNav/>
+          {profile !== null && profile.role === 'admin' && <AdminNav/>}
+        </nav>
+        <div className="user-view__content">
+          <div className="user-view__form-container">
+            <h2 className="heading-secondary ma-bt-md">Your account settings</h2>
+            <ProfileForm profile={profile} />
+          </div>
+          <div className="line">&nbsp;</div>
+          <div className="user-view__form-container">
+            <h2 className="heading-secondary ma-bt-md">Password change</h2>
+            <PasswordForm />
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-
-
-const mapStateToProps = createStructuredSelector({
-  profile: grabProfile,
-  loading: grabLoading
-});
-
-export default connect(mapStateToProps, null)(MeProfile);
+  )
+}
+export default MeProfile;
