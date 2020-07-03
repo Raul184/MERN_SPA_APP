49fc8b466b5dfc892b4adc4b46c1af394f4b3141
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-07-03 13:28:13 +0100

    Lazy & Suspense in place

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
+import React , {lazy,Suspense} from 'react';
 import {Switch,Route,Redirect} from "react-router-dom";
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
-import OverviewPage from './pages/Overview/OverviewPage';
-import TourPage from './pages/Tour/TourPage';
-import Login from './pages/Login/Login';
-import Signup from './pages/Signup/Signup';
-import MeProfile from './pages/Me/MeProfile';
+import Loading from './components/onLoading/OnLoading'
 import PrivateRoute from './components/auth/PrivateRoutes'
+
+const OverviewPage = lazy(() => import('./pages/Overview/OverviewPage'))
+const TourPage = lazy(() => import('./pages/Tour/TourPage'))
+const Login = lazy(() => import('./pages/Login/Login'))
+const Signup = lazy(() => import('./pages/Signup/Signup'))
+const MeProfile = lazy(() => import('./pages/Me/MeProfile'))
+
 function App() {
   return (
     <div className="App">
       <Header />
       <Switch>
+        <Suspense fallback={<Loading />}>
         <Route exact path='/' component={OverviewPage} />
         <Route exact path='/login' component={Login} />
         <Route exact path='/signup' component={Signup} />
         <Route path='/tours/:tourId' component={TourPage} />
         <PrivateRoute exact path='/me' component={MeProfile} />
         <Redirect to="/" />
+        </Suspense>
       </Switch>
       <Footer />
     </div>
