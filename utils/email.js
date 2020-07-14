91185108485c4994ad67e8baed010f602fb27ce0
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-07-14 15:22:58 +0100

    small detail fixed on mailsHandler

--- utils/email.js
+++ utils/email.js
@@ -13,7 +13,7 @@ const sendEmail = async options => {
 
   // 2) Define the email options
   const mailOptions = {
-    from: 'Jonas Schmedtmann <hello@jonas.io>',
+    from: 'Raul M. <rm@freakingTours.io>',
     to: options.email,
     subject: options.subject,
     text: options.message
