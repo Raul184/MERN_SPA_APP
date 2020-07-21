94c53b856faed1582379732dc1096d695781fb06
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-07-21 13:50:01 +0100

    Stripe payments completed , webhooks to be set after deployment

--- client/src/components/payWithStripe/PayStripe.js
+++ client/src/components/payWithStripe/PayStripe.js
@@ -4,15 +4,15 @@ import { loadStripe } from '@stripe/stripe-js';
 import {connect} from 'react-redux'
 import {createStructuredSelector} from 'reselect'
 import { grabProfile } from '../../redux/users/user.selectors';
+import  { Link } from 'react-router-dom'
 const stripePromise = loadStripe('pk_test_qpk8XXIK4nkQNnyLqJ3RH5Mt009GgCH5Lr');
 
 const PayStripe = ({match,userDb}) => {
   const handleClick = async () => {
-    console.log('clicked');
     try {
       // 1) Get checkout session from API
       const session = await axios(`/api/v1/bookings/checkout-session/${match.params.tourId}`);
-      console.log('CLICKED', session.data.session.id);
+  
       // 2) Create checkout form + charge credit card
       const stripe = await stripePromise;
       await stripe.redirectToCheckout({
@@ -23,16 +23,17 @@ const PayStripe = ({match,userDb}) => {
       console.log(err);
     }
   }
-  return (
-    <button 
-      className="btn btn--green span-all-rows"
-      onClick={handleClick}
-    >
-      <span>{userDb !== null ? 'Book Tour Now!' : 'Login to book'}</span>
-    </button>
-  )
+  return userDb !== null ? <button 
+    className="btn btn--green span-all-rows"
+    onClick={handleClick}
+  >
+    <span>Book Tour Now!</span>
+  </button> 
+  : 
+  <button className="btn btn--green span-all-rows">
+    <Link to='/login' style={{textDecoration:'none', color:'white'}}>Login to book</Link>
+  </button>  
 }
-
 
 const mapStateToProps = createStructuredSelector({
   userDb: grabProfile
